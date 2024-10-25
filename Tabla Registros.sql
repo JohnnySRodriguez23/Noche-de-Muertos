c51d5c1b65fe2af7a8010f6e46bc1ff66a73d0bb
JohnnySRodriguez23 <jr3414973@gmail.com>
2024-10-25 12:32:29 -0600

    Update Tabla Registros.sql

--- Tabla Registros.sql
+++ Tabla Registros.sql
@@ -1,4 +1,4 @@
-CREATE Table noche_de_muertos.registros (id_usuario INT PRIMARY KEY NOT NULL,
+CREATE Table Base_DM.registros (id_usuario INT PRIMARY KEY NOT NULL,
 Nombre VARCHAR(255) NOT NULL,
 Correo VARCHAR(255) NOT NULL,
 password VARCHAR(255) NOT NULL) ENGINE=INNODB;
